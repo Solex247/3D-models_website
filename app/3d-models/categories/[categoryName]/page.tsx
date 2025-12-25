e0373d5a249d6261bf0687553ede633eb0d3e729
Adams Solomon <solex247@gmail.com>
2025-12-25 09:32:37 +0100

    updates

--- app/3d-models/categories/[categoryName]/page.tsx
+++ app/3d-models/categories/[categoryName]/page.tsx
@@ -1,10 +1,10 @@
-import type { CategoryNamePageProps } from "@/app/types/Index";
+import type { CategoryPageProps } from "@/app/types/Index";
 import { getCategoryBySlug } from "@/app/lib/categories";
 import { getModels } from "@/app/lib/models";
 import type { Model } from "@/app/types/Index";
 import ModelsGrid from "@/app/components/ModelsGrid";
 
-export default async function CategoryPage({ params }: CategoryNamePageProps) {
+export default async function CategoryPage({ params }: CategoryPageProps) {
   const { categoryName } = await params;
   const category = getCategoryBySlug(categoryName);
   const getCategoryContent: Model[] = await getModels({
